3bdf2c8549b3a87d42cd3475db348e1f655cd6a4
EddyFizio <becerraallan2005@gmail.com>
2026-09-10 17:31:11 -0500

    Clase 10 Septiembre

--- src/main.pl
+++ src/main.pl
@@ -12,6 +12,7 @@ inventario('Elara' , [espada, escudo, pocion]).
 inventario('Kael' , [arco, flechas]).
 inventario('Rin' , [varita, grimorio, pocion, amuleto]).
 inventario('Allan', [escopeta, fusil]).
+inventario('Allan', [escopeta, fusil, 'Aplastadora']).
 
 requiere(m2, escudo). requiere(m2, pocion).
 requiere(m3, grimorio). requiere(m3, pocion).
@@ -19,4 +20,46 @@ requiere(m3, grimorio). requiere(m3, pocion).
 
 arma('Aplastadora', 59, 'Fuego').
 tiene('Allan', arma('Aplastadora', 59, hielo)).
-inventario('Allan', [escopeta, fusil, 'Aplastadora']).
+
+xp_para_subir(NivelActual, XP) :- 
+    XP is NivelActual * 30.
+
+vida_restante(VidaMax, Danio, Final) :-
+    Final is VidaMax - Danio.
+
+xp_acumulado(0,0).
+xp_acumulado(N, Total) :-
+    N > 0,
+    N1 is N - 1,
+    xp_acumulado(N1, Prev),
+    Total is Prev + (N * 30).
+
+
+danio_acumulado(0,0).
+danio_acumulado(N, Total) :-
+    N > 0,
+    N1 is N - 1,
+    danio_acumulado(N1, Prev),
+    Total is Prev + (N * 10).
+
+mismo_nivel(P1, P2) :-
+    personaje(P1, N, _),
+    personaje(P2, N, _),
+    P1 \== P2.
+
+
+es_balanceado(P) :-
+    personaje(P, _, Vida),
+    Vida =:= 100.
+
+mas_fuerte(P1, P2) :-
+    personaje(P1, Nivel1, Vida1),
+    personaje(P2, Nivel2, Vida2),
+    Nivel1 > Nivel2.
+
+mismo_objeto(P1, P2, Objeto) :-
+    inventario(P1, Inventario1),
+    inventario(P2, Inventario2),
+    member(Objeto, Inventario1),
+    member(Objeto, Inventario2).
+
